834da9d537a2a5b21ade7365b9742eee0e5523d3
Leo Adberg <leo@adberg.com>
2024-02-08 00:24:45 -0500

    Add paris and london

--- scrape_beli.py
+++ scrape_beli.py
@@ -12,6 +12,8 @@ regions = [
     ("bay", "Bay Area", 37.7, -122.4, 2),
     ("ny", "New York", 40.7, -74, 5),
     ("japan", "Japan", 35.7, 139.7, 10),
+    ("london", "London", 51, 0, 2),
+    ("paris", "Paris", 49, 2, 2),
     ("georgia", "Georgia", 42, 44, 4),
     ("other", "Other", 0, 0, 10000),
 ]
